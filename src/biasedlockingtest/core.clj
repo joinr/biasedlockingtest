933003a26006a9341d466ef154b8162396413970
tom <tom@that-place>
2020-09-17 22:04:51 -0500

    Updated results.

--- src/biasedlockingtest/core.clj
+++ src/biasedlockingtest/core.clj
@@ -1,8 +1,11 @@
 (ns biasedlockingtest.core
   (:require [criterium.core :as c]))
 
+(defn seq-range [n]
+  (take n (iterate inc 0)))
+
 (defn seq-smash []
-  (reduce + (seq (range 100000))))
+  (reduce + (seq-range 100000)))
 
 (defn run-test []
   (with-out-str
